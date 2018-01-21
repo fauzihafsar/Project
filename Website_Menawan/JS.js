7f120d579f802211c602261e34877af9656dd5de
fauzihafsar <salmanalfauzi@gmail.com>
2018-01-21 18:06:24 +0700

    Website Menawan Corp

---
+++ Website_Menawan/JS.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+            var sideslider = $('[data-toggle=collapse-side]');
+            var sel = sideslider.attr('data-target');
+            var sel2 = sideslider.attr('data-target-2');
+            sideslider.click(function(event){
+                $(sel).toggleClass('in');
+                $(sel2).toggleClass('out');
+            });
+        });
+window.success = function(msg) {
+    var dom = '<div class="top-alert"><div class="alert alert-success alert-dismissible fade in " role="alert"><i class="glyphicon glyphicon-ok"></i> ' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button></div></div>';
+    var jdom = $(dom);
+    jdom.hide();
+	$("body").append(jdom);
+	jdom.fadeIn();
+	setTimeout(function() {
+		jdom.fadeOut(function() {
+			jdom.remove();
+		});
+	}, 6000);
+}
